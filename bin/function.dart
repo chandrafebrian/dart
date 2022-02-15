519a4306f6a8080b2f523a0de866532099d79c00
chandrafebrian <chandrafebrian99@gmail.com>
2022-02-15 18:57:13 +0700

    up
    Signed-off-by: chandrafebrian <chandrafebrian99@gmail.com>

--- bin/function.dart
+++ bin/function.dart
@@ -1,10 +1,30 @@
-void main(List<String> args) {
-  int perhitungan;
+// void main(List<String> args) {
+//   int perhitungan;
 
-  perhitungan = tambah(2, 3);
-  print(perhitungan);
+//   perhitungan = fungsitambah(2, 3);
+//   print(perhitungan);
+// }
+
+// // contoh membuat function
+// int fungsitambah(int number1, int number2) {
+//   return number1 + number2;
+// }
+
+//
+//
+// ------------------------------------------------\
+// contoh fungsi optional parameter
+void main(List<String> args) {
+  String manggilfungsi;
+
+  manggilfungsi = fungsiwithoptionalparameter('chandra ', 'halo madafaka ',
+      tambahan: 'anjay ');
+
+  print(manggilfungsi);
 }
 
-int tambah(int number1, int number2) {
-  return number1 + number2;
+// contoh fungsi optional parameter dengan {} sebagai pilihan bisa di isi nilai nya atau tidak
+String fungsiwithoptionalparameter(String dari, String pesan,
+    {String? tambahan}) {
+  return dari + pesan + ((tambahan != null) ? tambahan : "");
 }
