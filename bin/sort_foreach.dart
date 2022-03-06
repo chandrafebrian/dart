133f65b73ec538e3efac76627b7b00e832e53b31
chandrafebrian <chandrafebrian99@gmail.com>
2022-03-06 19:53:01 +0700

    up
    Signed-off-by: chandrafebrian <chandrafebrian99@gmail.com>

--- bin/sort_foreach.dart
+++ bin/sort_foreach.dart
@@ -10,7 +10,15 @@ void main(List<String> args) {
     PersonOrang('merchant', 88),
   ];
 
-  listperson.sort((p1, p2) => p1.age - p2.age);
+// * keyword sort dan compareTo berfungsi sebgai membandingkan nilai parameter String dari (p1 dengan p2) menjadi abjad alfabet
+// * p1.age.compareTo(p2.age) menjadikan nilai nya berurut dari yg terkecil
+  listperson.sort((p1, p2) {
+    if (p1.role.compareTo(p2.role) != 0) {
+      return (p1.role.compareTo(p2.role));
+    } else {
+      return p1.age.compareTo(p2.age);
+    }
+  });
 
   for (var element in listperson) {
     print(element.role + ' - ' + element.age.toString());
