969066b7667032140d4e734f518713162c302289
chandrafebrian <chandrafebrian99@gmail.com>
2022-03-12 22:57:45 +0700

    up
    Signed-off-by: chandrafebrian <chandrafebrian99@gmail.com>

---
+++ bin/var_dynamic.dart
@@ -0,0 +1,9 @@
+void main(List<String> args) {
+  // tipe dynamic adalah jika kita punya variable atau reference yg diberikan type 'dynamic' itu artinya dapat kita pasangkan pada object apa pun
+  dynamic myDynamic;
+
+  myDynamic = 3;
+  myDynamic = 'hallo chandra';
+
+  print(myDynamic);
+}
