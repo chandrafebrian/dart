d5fa174c45e4fe938b2997bc9844c6fa5d464578
chandrafebrian <chandrafebrian99@gmail.com>
2022-03-13 08:37:10 +0700

    up
    Signed-off-by: chandrafebrian <chandrafebrian99@gmail.com>

--- bin/var_dynamic.dart
+++ bin/var_dynamic.dart
@@ -1,5 +1,5 @@
 void main(List<String> args) {
-  // tipe dynamic adalah jika kita punya variable atau reference yg diberikan type 'dynamic' itu artinya dapat kita pasangkan pada object apa pun
+  // tipe dynamic adalah jika kita punya variable atau reference yg diberikan type 'dynamic' sama seperti int, string dll, itu artinya dapat kita pasangkan pada object apa pun
   dynamic myDynamic;
 
   myDynamic = 3;
