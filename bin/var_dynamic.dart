db0fe25bea3688ad1fd77a7732088ac39e5cff14
chandrafebrian <chandrafebrian99@gmail.com>
2022-03-13 08:58:50 +0700

    up
    Signed-off-by: chandrafebrian <chandrafebrian99@gmail.com>

--- bin/var_dynamic.dart
+++ bin/var_dynamic.dart
@@ -1,9 +1,24 @@
+// void main(List<String> args) {
+// tipe dynamic adalah jika kita punya variable atau reference yg diberikan type 'dynamic' sama seperti int, string dll, itu artinya dapat kita pasangkan pada object apa pun
+//   dynamic myDynamic;
+
+//   myDynamic = 3;
+//   myDynamic = 'hallo chandra';
+
+//   print(myDynamic);
+// }
+
+//
+//
 void main(List<String> args) {
-  // tipe dynamic adalah jika kita punya variable atau reference yg diberikan type 'dynamic' sama seperti int, string dll, itu artinya dapat kita pasangkan pada object apa pun
-  dynamic myDynamic;
+  // keyword var artinya memungkinkan variable kita ini untuk menunjuk pada object apapun di object tipe apapun juga , tapi hanya sekali di awal saja dan harus se tipe dengan object yg pertama kali kita masukan
+//  jika var sudah di isi sebagai int , selama nya menjadi int, tidak bisa dimasukan dengan tipe data yg lain lagi
+  var variableSaya = 12;
 
-  myDynamic = 3;
-  myDynamic = 'hallo chandra';
-
-  print(myDynamic);
+  print(variableSaya);
 }
+
+
+
+// kapan pakai dynamic : kalau variable saya ada kemungkinan bebas bisa di isi  nilai atau object apapun
+// kapan pakai var : kalau saya tidak tahu , di awal itu apa isi tipe data nya , tapi nanti kedepan nya tipe data nya sudah pasti baku dan jelas
