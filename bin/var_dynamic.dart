ab3d7d6c360f972a25da3a256b6cd5de7e33e3c6
chandrafebrian <chandrafebrian99@gmail.com>
2022-03-13 09:15:25 +0700

    up
    Signed-off-by: chandrafebrian <chandrafebrian99@gmail.com>

--- bin/var_dynamic.dart
+++ bin/var_dynamic.dart
@@ -13,12 +13,17 @@
 void main(List<String> args) {
   // keyword var artinya memungkinkan variable kita ini untuk menunjuk pada object apapun di object tipe apapun juga , tapi hanya sekali di awal saja dan harus se tipe dengan object yg pertama kali kita masukan
 //  jika var sudah di isi sebagai int , selama nya menjadi int, tidak bisa dimasukan dengan tipe data yg lain lagi
-  var variableSaya = 12;
 
-  print(variableSaya);
+  var variableSaya = Orangvardynamic();
+  // var variableSaya = 12;
+  // variableSaya = 'hello';
+  variableSaya.name;
+  print(variableSaya.name);
 }
-
-
 
 // kapan pakai dynamic : kalau variable saya ada kemungkinan bebas bisa di isi  nilai atau object apapun
 // kapan pakai var : kalau saya tidak tahu , di awal itu apa isi tipe data nya , tapi nanti kedepan nya tipe data nya sudah pasti baku dan jelas
+
+class Orangvardynamic {
+  String name = 'chandra';
+}
